bf47c7203107b239a66b1fcd71730fdec015aadf
k3mystra <programmer.coleslaw981@slmail.me>
2025-05-06 13:07:05 +0800

    feat: Add constructor to Environment

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,3 +1,15 @@
 #include "environment.h"
 
 using namespace std;
+
+Environment::Environment(
+    int maxStep,
+    Vector2D dimension,
+    vector<RobotParameter> robotParams
+) {
+    this->maxStep = maxStep;
+    this->dimension = dimension;
+
+    // Instantiate robots here
+    // and put into robotList
+}
--- src/environment.h
+++ src/environment.h
@@ -2,11 +2,18 @@
 #define ENVIRONMENT_H
 
 #include <vector>
+#include <string>
 
 #include "abstractRobot/robot.h"
 #include "vector2d.h"
 
 using namespace std;
+
+
+struct RobotParameter {
+    string name;
+    Vector2D position;
+};
 
 
 // Class to store everything related to the overall environment stuff
@@ -18,7 +25,11 @@ private:
     // Width and height of the battleground
     Vector2D dimension;
 
+    int maxStep;
+    int step;
+
 public:
+    Environment(int maxStep, Vector2D dimension, vector<RobotParameter> robotParams);
     bool isRobotHere(Vector2D position);
     bool isPositionAvailable(Vector2D position);
 };
