6adaf5d8485bbe37a87753c32386fe2801fa0c8d
k3mystra <programmer.coleslaw981@slmail.me>
2025-05-06 08:32:01 +0800

    feat: Add mock function to GenericRobot

--- src/genericRobot.h
+++ src/genericRobot.h
@@ -6,9 +6,18 @@
 #include "abstractRobot/thinkingRobot.h"
 #include "abstractRobot/seeingRobot.h"
 #include "abstractRobot/shootingRobot.h"
+#include "vector2d.h"
 
 class GenericRobot : public MovingRobot, public ThinkingRobot, public SeeingRobot, public ShootingRobot {
+protected:
+    Vector2D position;
 
+public:
+    GenericRobot(Vector2D initialPosition);
+
+    void die() override;
+    void gotHit() override;
+    void executeTurn() override;
 };
 
 #endif  // GENERIC_ROBOT_H
