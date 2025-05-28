da90a4562148461a03587bc7db0df51d0a8740af
k3mystra <programmer.coleslaw981@slmail.me>
2025-05-28 15:50:18 +0800

    feat: make compile command work

--- src/compile.py
+++ src/compile.py
@@ -17,15 +17,21 @@ cpp_files = []
 for p in patterns:
     cpp_files.extend(glob.glob(p))
 
-cpp_files_no_ext = [os.path.splitext(x)[0] for x in cpp_files]
+print(cpp_files)
+# filename without the directory or its extension
+base_filename = [os.path.splitext(os.path.basename(x))[0] for x in cpp_files]
 
 
 # Compile to object files
-obj_files = [f"{BUILD_FOLDER}/{x}.o" for x in cpp_files_no_ext]
+obj_files = [f"{BUILD_FOLDER}/{x}.o" for x in base_filename]
+print(obj_files)
 for i, f in enumerate(cpp_files):
     subprocess.run(["g++", f, "-c", "-o", obj_files[i]])
 
 # Final compile
 compile_command = ["g++"]
 compile_command.extend(obj_files)
-compile_command.extend(["-c", "main.exe"])
+compile_command.extend(["-o", "main.exe"])
+print(compile_command)
+
+subprocess.run(compile_command)
