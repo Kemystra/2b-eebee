97bf3961ff4300c51423da986d1779c367525dbb
JaxChoong <choongjax@gmail.com>
2025-05-28 11:01:18 +0800

    feat: lookResult now randomises between scout and look, scout() now returns relative coordinates

--- src/upgrades/scoutBot.h
+++ src/upgrades/scoutBot.h
@@ -2,12 +2,13 @@
 #define SCOUTBOT_H
 
 #include "../genericRobot.h"
-
 class ScoutBot:public GenericRobot{
     public:
         vector<Vector2D> scout();
+        void thinkAndExecute() override;
     protected:
         int scoutCount =3;
+        bool useScout = false;
 };
 
 #endif
