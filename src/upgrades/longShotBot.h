c4ad3d2628f78a351919c39211d4bdd15b61a776
KHY0924 <keehuiyee0924@gmail.com>
2025-05-29 12:46:13 +0800

    longShotBot: Add parseInput function

--- src/upgrades/longShotBot.h
+++ src/upgrades/longShotBot.h
@@ -1,11 +1,45 @@
 #ifndef LONGSHOTBOT_H
 #define LONGSHOTBOT_H
 
-#include "../genericRobot.h"
+#include <cmath>
+// #include "../genericRobot.h"
+#include "../src/abstractRobot/shootingRobot.h"
 
-class LongShotBot:public GenericRobot{
-    public:
-        void fire(int x, int y) override;
-        int getMaxFiringDistance() const override;
+using namespace std;
+
+class LongShotBot : public ShootingRobot {
+private:
+    int maxFiringDistance; 
+
+public:
+    LongShotBot(int initialShells) {
+        shellCount = initialShells;  // Assuming the base class uses shellCount
+        maxFiringDistance = 3; // Set the maximum firing distance to 3
+    }
+
+    void fire(int x, int y) override {
+        double distance = sqrt(x * x + y * y);
+
+        if(distance > maxFiringDistance) {
+            cout << "Target is out of range!" << endl;
+            return;
+        }
+
+        if (shellCount <= 0) {
+            cout << "No shells remaining!" << endl;
+            return;
+        }
+
+        shellCount--;
+    }
+
+    int getMaxFiringDistance() const override {
+        return maxFiringDistance;
+    }
+
+    int getBulletsPerShot() const override {
+        return 1; // LongShotBot fires one bullet per shot
+    }
 };
+
 #endif
